bc113a35db3555de91f3750f974e9f0d3f44043b
wangxiyuan <wangxiyuan1007@gmail.com>
2023-06-06 17:17:02 +0800

    Update unit test

--- tools/oos/test/common/test_gitee.py
+++ tools/oos/test/common/test_gitee.py
@@ -19,6 +19,14 @@ def _mocked_requests_get(url, headers=None):
     return MockResponse(url, headers)
 
 
+def _mocked_get_gitee_project_tree(owner, project, branch, access_token):
+    return {
+        "tree": [
+            {"path": "123.tar.gz" },
+        ]
+    }
+
+
 class TestGiteeAction(unittest.TestCase):
 
     @mock.patch('requests.get', side_effect=_mocked_requests_get)
@@ -27,3 +35,8 @@ class TestGiteeAction(unittest.TestCase):
         self.assertEqual(result['url'], 'https://gitee.com/api/v5/repos/test_owner/test_project/git/trees/test_branch')
         self.assertEqual(result['headers'], "{'Content-Type': 'application/json;charset=UTF-8'}")
         self.assertEqual(len(mock_get.call_args_list), 1)
+
+    @mock.patch('oos.common.gitee.get_gitee_project_tree', side_effect=_mocked_get_gitee_project_tree)
+    def test_get_gitee_project_version(self, mock_get_gitee_project_tree):
+        result = gitee.get_gitee_project_version('test_owner', 'test_project', 'test_branch')
+        self.assertEqual(result, '123')
