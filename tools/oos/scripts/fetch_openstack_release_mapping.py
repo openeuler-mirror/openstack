72d8d699971e26a047bdadd3cdd50f22629d10a3
lixiang_yewu <lixiang_yewu@cmss.chinamobile.com>
2024-01-08 01:04:15 +0000

    update tools/oos/scripts/fetch_openstack_release_mapping.py.
    Signed-off-by: lixiang_yewu <lixiang_yewu@cmss.chinamobile.com>

--- tools/oos/scripts/fetch_openstack_release_mapping.py
+++ tools/oos/scripts/fetch_openstack_release_mapping.py
@@ -23,30 +23,33 @@ releases = [
 all_res = dict()
 for release in releases:
     url = 'https://releases.openstack.org/' + release
-    url_os_content = requests.get(url, verify=True).content.decode()
+    try:
+        url_os_content = requests.get(url, verify=True).content.decode()
 
-    # get all links, which ends .tar.gz from HTML
-    links = re.findall(r'https://.*\.tar\.gz', url_os_content)
-    results = dict()
-    for pkg_link in links:
-        if f"{release}-last" in pkg_link:
-            # tempest plugin version names like "stein-last" should be skipped.
-            continue
-        # get name and package informations from link
-        tmp = pkg_link.split("/")
-        pkg_full_name = tmp[4]
-        pkg_name = pkg_full_name[0:pkg_full_name.rfind('-')]
-        pkg_ver = pkg_full_name[
-                    pkg_full_name.rfind('-') + 1:pkg_full_name.rfind('.tar')]
-        # check if package with version are in results,
-        # and check for higher version
-        if pkg_name not in results:
-            results[pkg_name] = pkg_ver
-        else:
-            # if current versions < new version, then update it
-            if version.parse(results.get(pkg_name)) < version.parse(pkg_ver):
+        # get all links, which ends .tar.gz from HTML
+        links = re.findall(r'https://.*\.tar\.gz', url_os_content)
+        results = dict()
+        for pkg_link in links:
+            if f"{release}-last" in pkg_link:
+                # tempest plugin version names like "stein-last" should be skipped.
+                continue
+            # get name and package informations from link
+            tmp = pkg_link.split("/")
+            pkg_full_name = tmp[4]
+            pkg_name = pkg_full_name[0:pkg_full_name.rfind('-')]
+            pkg_ver = pkg_full_name[
+                        pkg_full_name.rfind('-') + 1:pkg_full_name.rfind('.tar')]
+            # check if package with version are in results,
+            # and check for higher version
+            if pkg_name not in results:
                 results[pkg_name] = pkg_ver
-    all_res[release] = results
+            else:
+                # if current versions < new version, then update it
+                if version.parse(results.get(pkg_name)) < version.parse(pkg_ver):
+                    results[pkg_name] = pkg_ver
+        all_res[release] = results
+    except requests.exceptions.RequestException as e:
+            print(f"Error fetching data for {release}: {e}")
 
 with open('openstack_release.yaml', 'w') as fp:
     fp.write(yaml.dump(all_res))
